9db6a770cd1860f676ec359068641a35ccb0f7cf
anton_alley <anton.alley@gmail.com>
2022-09-07 19:39:05 -0600

    Initial Commit

---
+++ SM_A_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SM_A_ios
+//
+//  Created by Anton Alley on 9/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SM_A_ios/SM_A_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SM_A_iosApp.swift
+//  SM_A_ios
+//
+//  Created by Anton Alley on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SM_A_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
